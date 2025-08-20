193669fef2088f1ff1fe709fdc6095bd40c7b9a1
kmorshed31 <145775209+kmorshed31@users.noreply.github.com>
2025-08-20 17:36:23 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE = 'ble-client-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
